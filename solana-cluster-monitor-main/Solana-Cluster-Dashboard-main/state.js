8faefb58895b1333ef1969cc4c6fed3ca4a462af
RachitSrivastava12 <rachitsrivastava76@gmail.com>
2025-09-23 15:04:51 +0530

    Initial commit

--- solana-cluster-monitor-main/Solana-Cluster-Dashboard-main/state.js
+++
@@ -1,8 +0,0 @@
-// // state.js (inferred and added as it's required but not provided in the query)
-// module.exports = {
-//   isPollingStarted: false,
-// };
-
-module.exports = {
-  isPollingStarted: false,
-};
